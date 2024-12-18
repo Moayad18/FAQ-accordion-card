370b974f832b5a4e235663e994a88f482a213cec
Moayad18 <98690386+Moayad18@users.noreply.github.com>
2024-12-18 09:17:54 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+const questions = document.querySelectorAll(".question");
+
+questions.forEach((question) => {
+  const btn = question.querySelector("img");
+  const questionText = question.querySelector("p");
+  btn.addEventListener("click", () => {
+    questions.forEach((item) => {
+      if (item !== question) {
+        item.classList.remove("show-text");
+      }
+    });
+    question.classList.toggle("show-text");
+  });
+  questionText.addEventListener("click", () => {
+    questions.forEach((item) => {
+      if (item !== question) {
+        item.classList.remove("show-text");
+      }
+    });
+    question.classList.toggle("show-text");
+  });
+});
